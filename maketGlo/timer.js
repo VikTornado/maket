3d613db1c9017441d3aecfbc815e36402e1ffea7
React <3809164@gmail.com>
2022-09-11 10:53:36 +0300

    c
    c

--- maketGlo/timer.js
+++ maketGlo/timer.js
@@ -1,9 +1,1 @@
-let timer = (deadline) => {
-  let timerHours = document.getElementById("timer-hours");
-  let timerMinutes = document.getElementById("timer-minutes");
-  let timerSeconds = document.getElementById("timer-seconds");
 
-  timerHours.textContent = new Date(deadline, timerHours);
-};
-
-timer("08 september 2022");
